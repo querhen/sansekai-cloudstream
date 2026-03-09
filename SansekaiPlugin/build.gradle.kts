c850076202793369fa8a9248a6d6455e7933eb27
querhen <querhen@gmail.com>
2026-03-09 11:20:54 +0700

    Update build.gradle.kts

--- SansekaiPlugin/build.gradle.kts
+++ SansekaiPlugin/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-
 plugins {
     id("com.android.library")
     kotlin("android")
@@ -9,14 +7,10 @@ plugins {
 android {
     namespace = "com.querhen.sansekai"
     compileSdk = 33
-
-    defaultConfig {
-        minSdk = 21
-    }
+    defaultConfig { minSdk = 21 }
 }
 
 cloudstream {
-    // Ini akan otomatis membuat file .cs3 yang bisa dibaca aplikasi
-    setPath("com.querhen.Sansekai") 
-    description = "Nonton Drama China, Shortmax, dan Anime gratis dari Sansekai API"
+    setPath("com.querhen.Sansekai")
+    description = "Nonton Drama & Anime gratis dari Sansekai API"
 }

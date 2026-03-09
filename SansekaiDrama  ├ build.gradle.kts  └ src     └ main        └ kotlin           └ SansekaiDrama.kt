ec16495ec330385436edb497c36e3bb410667f5b
querhen <querhen@gmail.com>
2026-03-09 07:00:21 +0700

    Create SansekaiDrama  ├ build.gradle.kts  └ src     └ main        └ kotlin           └ SansekaiDrama.kt

---
+++ SansekaiDrama  ├ build.gradle.kts  └ src     └ main        └ kotlin           └ SansekaiDrama.kt
@@ -0,0 +1,11 @@
+plugins {
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+cloudstream {
+    language = "id"
+    description = "Sansekai Short Drama Provider"
+    authors = listOf("querhen")
+    status = 1
+    tvTypes = listOf("TvSeries")
+}

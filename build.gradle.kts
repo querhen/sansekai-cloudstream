806d5801a20913c0107b7183dec4f8ec00f5e573
querhen <querhen@gmail.com>
2026-03-09 13:06:14 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,3 @@ plugins {
     id("com.lagradost.cloudstream3.gradle") version "6.0.2" apply false
     kotlin("android") version "1.9.22" apply false
 }
-
-// Tidak perlu blok buildscript atau allprojects lagi karena sudah di-handle settings.gradle.kts

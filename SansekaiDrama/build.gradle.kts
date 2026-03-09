9858df85ec9092121c2f40bc14ddf1d3e2e4c5f2
querhen <querhen@gmail.com>
2026-03-09 07:04:31 +0700

    Create build.gradle.kts

---
+++ SansekaiDrama/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+cloudstream {
+    language = "id"
+    description = "Sansekai Short Drama"
+    authors = listOf("querhen")
+    status = 1
+    tvTypes = listOf("TvSeries")
+}
